e7c2556034a8466970c4309a3fbb73a97a35ad5f
kushwant Singh Rajawat <168860456+khushwant-Singh-Rajawat@users.noreply.github.com>
2026-04-28 22:56:38 +0530

    Update SecurityConfig.java

--- src/main/java/com/RajawatElectrical/config/SecurityConfig.java
+++ src/main/java/com/RajawatElectrical/config/SecurityConfig.java
@@ -14,16 +14,11 @@ public class SecurityConfig
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth
-                        // ✅ Public pages
-                        .requestMatchers("/", "/css/**", "/js/**", "/images/**", "/login.html", "/register.html").permitAll()
-                        // ✅ Protected APIs (Need Login)
-                        .requestMatchers("/api/cart/**", "/api/services/book", "/api/orders/**").authenticated()
-                        // ✅ All other APIs public
-                        .anyRequest().permitAll()
-                );
-        return http.build();
+                 http
+        .csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(auth -> auth
+            .anyRequest().permitAll()
+        );
+    return http.build();
     }
 }
